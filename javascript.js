d7a3c7b571d39a3887fe55cbf7778c4a39382cfb
Spxchaos <amannaiknov@gmail.com>
2026-03-11 16:39:34 +0100

    built a logic for computer input using Math.Random and if function

--- javascript.js
+++ javascript.js
@@ -8,28 +8,47 @@ function getHumanChoice(){
 
     const x = prompt("Rock, Paper, or Scissors:");
 
-        y = x.toLowerCase();
+        let y = x.toLowerCase(); //to make the input case sensitive
 
-        if(y == 'rock' || y == 'paper' || y == 'scissors'){
+        if(y == 'rock' || y == 'paper' || y == 'scissors'){  //if loop is in place so that the user cant enter invalid input, the function would run only if the inputs are correct
 
         return y;
         }
 
         else{
-            getHumanChoice();
+            getHumanChoice();  //this runs the function again
         }
 
 }
 
-/*function getComputerChoice(){
+function getComputerChoice(){
+
+    let z = Math.random() * 10;
+    console.log(z);
+
+    if(z>= 0 && z<= 3){
+     console.log('rock');
+    }
+
+    else if (z>=3 && z<=6){
+        console.log('paper');
+    }
+
+    else
+    {
+        console.log('scissors');
+    }
+
 
 
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+//const humanSelection = getHumanChoice();
+//const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);*/
+//playRound(humanSelection, computerSelection);
 
-getHumanChoice();
+//getHumanChoice();
+
+getComputerChoice();
